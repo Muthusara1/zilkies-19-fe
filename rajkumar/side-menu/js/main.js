dea613247d0ed6c34e6cb905f08729062c014fb8
Raj <rajkumar.cind@gmail.com>
2018-08-27 12:07:18 +0530

    form-validation

---
+++ rajkumar/side-menu/js/main.js
@@ -0,0 +1,9 @@
+function openSideBar() {
+    document.getElementById("side-nav-id").style.width = "250px";
+    document.getElementById("main-body-section-id").style.marginLeft = "250px";
+}
+
+function closeSideBar() {
+    document.getElementById("side-nav-id").style.width = "0";
+    document.getElementById("main-body-section-id").style.marginLeft = "0";
+}

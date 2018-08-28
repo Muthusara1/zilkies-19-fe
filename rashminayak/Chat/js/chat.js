df20ed1d1fb0e167ac9e3ab1ee9fe917851a9bed
rashmi <15eucs107@skcet.ac.in>
2018-08-28 11:12:30 +0530

    Appended new messages to chat

--- rashminayak/Chat/js/chat.js
+++ rashminayak/Chat/js/chat.js
@@ -5,20 +5,24 @@ function addMessages() {
     var hour = date.getHours();
     var min = date.getMinutes();
     var url = "http://localhost:3000/posts";
+    var meridian;
+    if(hour > 11) 
+        meridian = "PM";
+    else
+        meridian = "AM";
     fetch(url,{
         method: "POST",
         headers : {
             "Accept" : "application/json",
             "Content-Type" : "application/json"
         },
-        body: JSON.stringify({"author":"rashmi", "message": message, "time":hour+":"+min})       
+        body: JSON.stringify({"author":"rashmi", "message": message, "time":hour+":"+min+meridian})       
     }).then(function(response){
-        alert("Stored!");
         var chatbox = document.getElementById("chat-box");
-        var pElement = document.createElement("p");
-        pElement.innerHTML = "From script";
-        chatbox.append(pElement);
-        printHello();
+        var newMessage = document.createElement("div");
+        newMessage.setAttribute("class","message-container");
+        newMessage.innerHTML += "<div class='message-content'><p class='message'>"+message+"</p><p class='time'>"+hour+":"+min+meridian+"</p></div><i class='fas fa-headset' style='color: black;'></i>";
+        chatbox.append(newMessage);
     }).catch(function(error){
         alert("Error!");
     });

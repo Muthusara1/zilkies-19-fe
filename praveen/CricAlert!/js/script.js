353805f34f44aae0be58baa7be58c6047eaecded
Praveen Kumar <dpraveenk38@gmail.com>
2018-08-29 22:49:46 +0530

    Changed fade effect to fade into (merge effect)

--- praveen/CricAlert!/js/script.js
+++ praveen/CricAlert!/js/script.js
@@ -5,13 +5,16 @@ function activeToggle(selection) {
     var signUpForm = document.getElementById("credentials__signup-id");
 
     if (selection == "login" && login.classList.contains("credentials__title__inactive")) {
+        signUpForm.classList.remove("fade-in");
         signUpForm.classList.add("fade-out");
+        loginForm.classList.remove("fade-out");
+        loginForm.classList.add("fade-in");
 
-        setTimeout(function doThis() {
-            loginForm.style.display = "flex";
-            signUpForm.style.display = "none";
-            loginForm.classList.add("fade-in");
-        }, 500);
+        // setTimeout(function doThis() {
+        //     loginForm.style.display = "flex";
+        //     signUpForm.style.display = "none";
+        //     loginForm.classList.add("fade-in");
+        // }, 500);
 
         // signUp.classList.remove("fade-in");
         // signUp.classList.add("fade-out");
@@ -23,31 +26,21 @@ function activeToggle(selection) {
         signUp.classList.remove("credentials__title__active");
         signUp.classList.add("credentials__title__inactive");
 
-        setTimeout(function toggleDisplay() {
-            signUpForm.classList.remove("fade-out");
-            loginForm.classList.remove("fade-in");
-            
-
-        }, 1000);
+        // setTimeout(function toggleDisplay() {
+        //     signUpForm.classList.remove("fade-out");
+        //     loginForm.classList.remove("fade-in");
+        // }, 1000);
     } else if (selection == "signUp" && signUp.classList.contains("credentials__title__inactive")) {
+        loginForm.classList.remove("fade-in");
         loginForm.classList.add("fade-out");
-        login.classList.add("fade-out");
-        signUp.classList.add("fade-out");
+        signUpForm.classList.remove("fade-out");
+        signUpForm.classList.add("fade-in");
 
-        setTimeout(function doThis() {
-            login.classList.remove("fade-out");
-            signUp.classList.remove("fade-out");
-            signUp.classList.remove("credentials__title__inactive");
-            signUp.classList.add("credentials__title__active");
-            login.classList.remove("credentials__title__active");
-            login.classList.add("credentials__title__inactive");
-    
-            loginForm.style.display = "none";
-            signUpForm.style.display = "flex";
-            signUpForm.classList.add("fade-in");
-            login.classList.add("fade-in");
-            signUp.classList.add("fade-in");
-        }, 500);
+        // setTimeout(function doThis() {
+        //     loginForm.style.display = "none";
+        //     signUpForm.style.display = "flex";
+        //     signUpForm.classList.add("fade-in");
+        // }, 500);
 
         // login.classList.remove("fade-in");
         // void login.offsetWidth;
@@ -56,9 +49,15 @@ function activeToggle(selection) {
         // void signUp.offsetWidth;
         // signUp.classList.add("fade-in");
 
-        setTimeout(function toggleDisplay() {
-            loginForm.classList.remove("fade-out");
-            signUpForm.classList.remove("fade-in");
-        }, 1000);
+        signUp.classList.remove("credentials__title__inactive");
+        signUp.classList.add("credentials__title__active");
+        login.classList.remove("credentials__title__active");
+        login.classList.add("credentials__title__inactive");
+
+        // setTimeout(function toggleDisplay() {
+        //     loginForm.style.display = "none";
+        //     loginForm.classList.remove("fade-out");
+        //     signUpForm.classList.remove("fade-in");
+        // }, 1000);
     }
 }

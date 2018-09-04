f65e1208ca24ee44c2ab460ac003ca0b7f0a71cf
Praveen Kumar <dpraveenk38@gmail.com>
2018-09-04 16:08:07 +0530

    Added more functionality to cricalert

--- praveen/CricAlert!/js/script.js
+++ praveen/CricAlert!/js/script.js
@@ -1,3 +1,5 @@
+// document.getElementsByClassName("content__player-card").onclick = expandCard;
+
 function activeToggle(selection) {
     var login = document.getElementById("credentials__title__login-id");
     var loginForm = document.getElementById("credentials__login-id");
@@ -41,4 +43,93 @@ function activeToggle(selection) {
             signUpForm.classList.remove("fade-in");
         }, 500);
     }
+}
+
+function validateSignUpForm() {
+    var nameRegex = /^[a-zA-Z ]{2,30}$/;
+    var emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    var name = document.forms["signup-form"]["name"].value;
+    var email = document.forms["signup-form"]["email-id"].value;
+    var username = document.forms["signup-form"]["username"].value;
+    var password = document.forms["signup-form"]["password"].value;
+    console.log("Inside!")
+    if (!nameRegex.test(name)) {
+        alert("Invalid name entered!");
+        return false;
+    }
+    if (!emailRegex.test(email)) {
+        alert("Invalid email entered!");
+        return false;
+    }
+    if (username == "") {
+        alert("Username is required!");
+        return false;
+    }
+    if (password == "") {
+        alert("Password is required!");
+        return false;
+    }
+    return true;
+}
+
+function validateLoginForm() {
+    var username = document.forms["login-form"]["username"].value;
+    var password = document.forms["login-form"]["password"].value;
+    if (username == "") {
+        alert("Username is required!");
+        return false;
+    }
+    if (password == "") {
+        console.log("Hmmm...")
+        alert("Password is required!");
+        return false;
+    }
+    return true;
+}
+
+function expandCard(event) {
+    var element = event.target;
+    if (element.classList.contains("col-sm-8")) {
+        element.classList.remove("col-sm-8");
+        element.classList.add("col-sm-4");
+        element.style.height = "100px";
+    } else {
+        var nodeList = document.querySelectorAll(".content__player-card");
+        for (var i  = 0; i < nodeList.length; i++) {
+            nodeList[i].classList.remove("col-sm-8");
+            nodeList[i].classList.add("col-sm-4");
+            nodeList[i].style.height = "100px";
+        }
+        element.style.backgroundColor = "wheat";
+        element.style.height = 2 * element.clientHeight + "px";
+        element.classList.remove("col-sm-4");
+        element.classList.add("col-sm-8");    
+    }
+}
+
+function expandSearch() {
+    var searchIcon = document.querySelector(".footer__search");
+    searchIcon.style.display = "none";
+    var searchIcon = document.querySelector(".add-item");
+    searchIcon.style.display = "none";
+    var searchBar = document.querySelector(".footer__search-bar");
+    searchBar.style.display = "block";
+    var minimizeIcon = document.querySelector(".minimize");
+    minimizeIcon.style.display = "inline-block";
+}
+
+function minimizeSearch() {
+    var searchIcon = document.querySelector(".footer__search");
+    searchIcon.style.display = "inline-block";
+    var searchIcon = document.querySelector(".add-item");
+    searchIcon.style.display = "inline-block";
+    var searchBar = document.querySelector(".footer__search-bar");
+    searchBar.style.display = "none";
+    var minimizeIcon = document.querySelector(".minimize");
+    minimizeIcon.style.display = "none";
+}
+
+function sideBarClick() {
+    console.log("sideBarClick!")
+    var sideBar = document.querySelector(".side-bar").classList.toggle('active');
 }
